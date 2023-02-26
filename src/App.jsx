8b4701b2d5ad301a4d6217b21e90d2b7af83ccec
Ihtisham Ul haq <you@example.com>
2023-02-26 23:22:15 +0500

    code refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
-import Chat from "../components/chat";
-import ChatsAll from "../components/ChatsAll";
 import SignIn from "../components/SignIn";
 import ChatApp from "../components/chatApp";
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
 function App() {
-  // const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
-  const [isAuth, setIsAuth] = useState(true);
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  // const [isAuth, setIsAuth] = useState(true);
   return (
     <>
       {!isAuth ? (
